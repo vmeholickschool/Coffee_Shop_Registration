a9dca9832fe98f40c43696c8f3e17fc9a6ea3e9a
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-04-13 10:44:43 -0400

    Add project files.

---
+++ Coffee_Shop_Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_Shop_Registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstName, string email, string password)
+        {
+            ViewData["FirstName"] = firstName;
+            return View("Result");
+        }
+    }
+}
